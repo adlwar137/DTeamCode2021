eaf9ab76c029542dbbada985ddcbf2bfab308758
Adler <adlerwarmbier@gmail.com>
2021-01-24 21:42:12 -0500

    basic rectangle functionality

--- include/graphics.h
+++ include/graphics.h
@@ -3,6 +3,8 @@
 //this is meant to be a complete rewrite of drawing from the ground up
 //for the sins i am about to commit please forgive me Bjarne Stroustrup
 
+//TODO line class
+
 namespace graphics {
   class Point {
     public:
@@ -10,11 +12,24 @@ namespace graphics {
       int y;
       int z;
       Point(int x = 0, int y = 0, int z = 0); //constructor
-      int GetProjectedX(double d);
-      int GetProjectedY(double d);
-      void drawPoint(double d); //render point
+      int GetProjectedX(double fov);
+      int GetProjectedY(double fov);
+      void draw(double fov); //render point
     //override add vector to point
     //override subtract vector from point
     //override subtractpointfrompoint
+  };
+
+  class Rectangle {
+    public:
+      int x;
+      int y;
+      int z;
+      int width;
+      int height;
+      Rectangle(int x = 0, int y = 0, int z = 0, int width = 0, int height = 0); //constructor
+      void draw(double fov);
+    private:
+      Point points[4];
   };
 }
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,5 +1,5 @@
 #include "graphics.h"
-
+//Point
 //Point Constructor
 graphics::Point::Point(int x, int y, int z) {
   this->x = x;
@@ -7,14 +7,42 @@ graphics::Point::Point(int x, int y, int z) {
   this->z = z;
 }
 
-int graphics::Point::GetProjectedX(double d) {
-  return ceil(this->x * (d / this->z));
+int graphics::Point::GetProjectedX(double fov) {
+  return ceil(this->x * (fov / this->z));
 }
 
-int graphics::Point::GetProjectedY(double d) {
-  return ceil(this->x * (d / this->z));
+int graphics::Point::GetProjectedY(double fov) {
+  return ceil(this->x * (fov / this->z));
 }
 
-void graphics::Point::drawPoint(double d) {
-  Brain.Screen.drawPixel(this->GetProjectedX(d), this->GetProjectedY(d));
+void graphics::Point::draw(double fov) {
+  Brain.Screen.drawPixel(this->GetProjectedX(fov), this->GetProjectedY(fov));
+}
+
+//Rectangle
+graphics::Rectangle::Rectangle(int x, int y, int z, int width, int height) {
+  this->x = x;
+  this->y = y;
+  this->z = z;
+  this->width = width;
+  this->height = height;
+
+  this->points[0].x = this->x;
+  this->points[0].y = this->y;
+
+  this->points[1].x = this->width;
+  this->points[1].y = this->y;
+
+  this->points[2].x = this->x;
+  this->points[2].y = this->height;
+
+  this->points[3].x = this->width;
+  this->points[3].y = this->height;
+}
+
+//TODO draw lines inbetween the points
+void graphics::Rectangle::draw(double fov) {
+  for(int i = 0; i < sizeof(this->points); i++) {
+    points[i].draw(fov);
+  }
 }
