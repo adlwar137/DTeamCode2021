8fa3eace2afc6e0b6d4d38d4eeda2f7112b660a4
Adler <adlerwarmbier@gmail.com>
2021-01-24 23:18:01 -0500

    WIP graphics

--- include/graphics.h
+++ include/graphics.h
@@ -8,16 +8,44 @@
 namespace graphics {
   class Point {
     public:
-      int x;
-      int y;
-      int z;
       Point(int x = 0, int y = 0, int z = 0); //constructor
       int GetProjectedX(double fov);
       int GetProjectedY(double fov);
       void draw(double fov); //render point
+      //Getters
+      int getX();
+      int getY();
+      int getZ();
+      //Setters
+      void setX(int x);
+      void setY(int y);
+      void setZ(int z);
+    private:
+      int x;
+      int y;
+      int z;
     //override add vector to point
     //override subtract vector from point
     //override subtractpointfrompoint
+  };
+
+  class Line {
+    public:
+      Line(int x = 0, int y = 0, int z = 0, int x1 = 0, int y1 = 0, int z1 = 0); //constructor
+      void draw(double fov); //render line
+      //Getters
+      int getX();
+      int getY();
+      int getZ();
+      int getX1();
+      int getY1();
+      int getZ1();
+      Point getP1();
+      Point getP2();
+      //Setters
+    private:
+      Point p1;
+      Point p2;
   };
 
   class Rectangle {
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,23 +1,74 @@
 #include "graphics.h"
 //Point
-//Point Constructor
+//Constructor
 graphics::Point::Point(int x, int y, int z) {
   this->x = x;
   this->y = y;
   this->z = z;
 }
 
+//get the projected x value to draw to the screen
 int graphics::Point::GetProjectedX(double fov) {
   return ceil(this->x * (fov / this->z));
 }
 
+//get the projected y value to draw to the screen
 int graphics::Point::GetProjectedY(double fov) {
   return ceil(this->x * (fov / this->z));
 }
 
+//draw the point to the screen
 void graphics::Point::draw(double fov) {
   Brain.Screen.drawPixel(this->GetProjectedX(fov), this->GetProjectedY(fov));
 }
+
+//Getters
+int graphics::Point::getX() {
+  return this->x;
+}
+int graphics::Point::getY() {
+  return this->y;
+}
+int graphics::Point::getZ() {
+  return this->z;
+}
+//Setters
+void graphics::Point::setX(int x) {
+  this->x = x;
+}
+void graphics::Point::setY(int y) {
+  this->y = y;
+}
+void graphics::Point::setZ(int z) {
+  this->z = z;
+}
+
+//Line
+//Constructor
+graphics::Line::Line(int x, int y, int z, int x1, int y1, int z1) {
+  Point p1(x,y,z);
+  Point p2(x1,y1,z1);
+}
+//Getters
+int graphics::Line::getX() {
+  return this->p1.getX();
+}
+int graphics::Line::getY() {
+  return this->p1.getY();
+}
+int graphics::Line::getZ() {
+  return this->p1.getZ();
+}
+int graphics::Line::getX1() {
+  return this->p2.getX();
+}
+int graphics::Line::getY1() {
+  return this->p2.getY();
+}
+int graphics::Line::getZ1() {
+  return this->p2.getZ();
+}
+//Setters
 
 //Rectangle
 graphics::Rectangle::Rectangle(int x, int y, int z, int width, int height) {
@@ -27,17 +78,17 @@ graphics::Rectangle::Rectangle(int x, int y, int z, int width, int height) {
   this->width = width;
   this->height = height;
 
-  this->points[0].x = this->x;
-  this->points[0].y = this->y;
+  this->points[0].setX(this->x);
+  this->points[0].setY(this->y);
 
-  this->points[1].x = this->width;
-  this->points[1].y = this->y;
+  this->points[1].setX(this->width);
+  this->points[1].setY(this->y);
 
-  this->points[2].x = this->x;
-  this->points[2].y = this->height;
+  this->points[2].setX(this->x);
+  this->points[2].setY(this->height);
 
-  this->points[3].x = this->width;
-  this->points[3].y = this->height;
+  this->points[3].setX(this->width);
+  this->points[3].setY(this->height);
 }
 
 //TODO draw lines inbetween the points
