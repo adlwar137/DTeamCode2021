a6506eca3522360eda28932feccfbbde4bdbe058
Adler Warmbier <adlerwarmbier@gmail.com>
2021-03-28 23:23:01 -0400

    match right auton changes

--- src/main.cpp
+++ src/main.cpp
@@ -361,14 +361,16 @@ Drivetrain.driveFor(reverse, 40, vex::distanceUnits::in, 20, vex::velocityUnits:
 //  Auton::turnToHeading(180, 1, 1, 15);
 
 //  Drivetrain.driveFor(forward, 70, distanceUnits::cm, 10, vex::velocityUnits::pct);
-
-  //Match Right
 /*
-  Auton::rollerTrainForTime(2, forward, seconds, 80, vex::velocityUnits::pct);
+  //Match Right
+  Auton::driveForTime(0.5, fwd, seconds, 20, velocityUnits::pct);
+  //Auton::intakeOn();
+  Auton::rollerTrainForTime(1.5, forward, seconds, 80, vex::velocityUnits::pct);
+  //Auton::intakeOff();
   Drivetrain.driveFor(reverse, 18, vex::distanceUnits::cm, 10, vex::velocityUnits::pct);
-  Auton::turnToHeading(270, 1, 1, 25);
-  Drivetrain.driveFor(forward, 39, vex::distanceUnits::cm, 10, vex::velocityUnits::pct);
-  Auton::turnToHeading(315, 1, 1, 25);
+  Auton::turnToHeading(270, 1, 1, 25, 1);
+  Drivetrain.driveFor(forward, 39, vex::distanceUnits::cm, 20, vex::velocityUnits::pct);
+  Auton::turnToHeading(315, 1, 1, 25, 1);
   Auton::intakeOn();
   Auton::driveForTime(2, forward, seconds, 20, velocityUnits::pct);
   Auton::intakeOff();
